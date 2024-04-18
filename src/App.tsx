413643f73db384dfa8ceb8e143275dfd4c9a6112
JAmal Michael <eshimi090@gmail.com>
2024-04-18 06:15:23 -1200

    adding shadcn config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AuthLayout from './_auth/AuthLayout';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
 import { Home } from './_root/pages';
 import './globals.css';
 import { Routes, Route } from 'react-router-dom';
@@ -17,7 +18,11 @@ const App = () => {
       
 
             {/* private routes */}
+            <Route element={<RootLayout/>}>
             <Route index element={<Home />}/>
+
+            </Route>
+
         </Routes>
     </main>
   )
