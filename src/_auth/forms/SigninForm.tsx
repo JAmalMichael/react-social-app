df9a3e768d8aef87ce86198701bdfa6615562802
JAmal Michael <eshimi090@gmail.com>
2024-04-18 05:16:36 -1200

    adding forms pages and auth routes.

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SininForms() {
+  return (
+    <div>SininForms</div>
+  )
+}
+
+export default SininForms
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingupForm() {
+  return (
+    <div>SingupForm</div>
+  )
+}
+
+export default SingupForm
