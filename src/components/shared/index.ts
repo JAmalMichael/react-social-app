fab6d0abc1e66fe816b644165238ab7934cc4ffc
JAmal Michael <eshimi090@gmail.com>
2024-04-28 08:37:36 -1200

    fileuploader component.

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { default as Bottombar } from "./BottomBar";
 export { default as LeftSidebar } from "./LeftSideBar";
 export { default as Loader } from "./Loader";
 export { default as Topbar } from "./Topbar";
+// export { default as FileUploader } from "./FileUploader";
