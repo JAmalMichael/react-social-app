6dd49a7978d5b460fd73362cdafa9286dc82d66a
JAmal Michael <eshimi090@gmail.com>
2024-05-01 04:43:27 -1200

    Poststats component

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,8 +1,60 @@
+import { useUserContext } from "@/context/AuthContext";
+import { useLikePost, useSavePost, useDeleteSavedPost } from "@/lib/react-query/queriesandMutations";
+import { checkIsLiked } from "@/lib/utils";
+import { Models } from "appwrite"
+import { useState, useEffect } from "react";
 
 
-const PostStats = () => {
+
+type PostStatsProps = {
+    post: Models.Document;
+    userId: string;
+}
+const PostStats = ({post, userId}: PostStatsProps) => {
+
+  const likesList = post.likes.map((user: Models.Document) => user.$id)
+
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const{mutate: likePost} = useLikePost();
+  const{mutate: savePost} = useSavePost();
+  const{mutate: deleteSavedPost} = useDeleteSavedPost();
+
+  // const {data: currentUser} = useGetCurrentUser();
+
+  const handleLikePost = () => {}
+
+  const handleSavePost = () => {}
+
+
   return (
-    <div>PostStats</div>
+    <div className="flex justify-between items-center z-20">
+        <div className="flex gap-2 mr-5">
+            <img 
+            src={`${checkIsLiked(likes, userId)
+             ? "/assets/icons/liked.svg"
+            : "/assets/icons/like.svg"}`}
+            alt="like"
+            width={20}
+            height={20}
+            onClick={handleLikePost}
+            className="curso-pointer"
+            />
+            <p className="small-medium lg:base-medium">{likes.length}</p>
+        </div>
+
+        <div className="flex gap-2">
+            <img 
+            src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
+            alt="like"
+            width={20}
+            height={20}
+            onClick={handleSavePost}
+            className="curso-pointer"
+            />
+        </div>
+    </div>
   )
 }
 
