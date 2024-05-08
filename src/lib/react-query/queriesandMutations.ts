e5f2aa7f90d531af3760eed6177f06155663949c
JAmal Michael <eshimi090@gmail.com>
2024-05-07 13:41:14 -1200

    Gridpost list

--- src/lib/react-query/queriesandMutations.ts
+++ src/lib/react-query/queriesandMutations.ts
@@ -146,18 +146,18 @@ export const useCreatePost = () => {
     })
   }
 
-  // export const useGetPosts = () => {
-  //   return useInfiniteQuery({
-  //     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-  //     queryFn: getInfinitePosts,
-  //     getNextPageParam: (lastPage) => {
-  //       if (lastPage && lastPage.documents.length === 0) {  return null;
-  //       }
-  //       const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
-  //       return lastId;
-  //     },
-  //   });
-  // };
+  export const useGetPosts = () => {
+    return useInfiniteQuery({
+      queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+      queryFn: getInfinitePosts,
+      getNextPageParam: (lastPage) => {
+        if (lastPage && lastPage.documents.length === 0) {  return null;
+        }
+        const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+        return lastId;
+      },
+    });
+  };
 
   export const userSearchPosts = (searchTerm: string) => {
     return useQuery({
